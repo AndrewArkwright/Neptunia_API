1cb62adf235e8c67d1c19fe490ed27942994c658
andrew arkwright <andrew.arkwright@hotmail.com>
2022-06-07 19:50:24 -0400

    Add more properties to objects and finish up the four CPUs

--- server.js
+++ server.js
@@ -7,28 +7,77 @@ app.use(cors())
 
 let characters = {
     'neptune':{
-        "Name": "Neptune",
+        "English Name": "Neptune",
+        "Japanese Name": "ネプテューヌ",
+        "Romanji Name": "Neputūnu",
         "Nicknames": ["Neppy", "Neptuna", "Nep Nep", "Nep", "Nep-Ko", "Nep-chan", "Nepsy", "Sis", "Small Neptune", "Crapbike"],
         "Title": "CPU",
-        "Race": "Goddess"
+        "Occupation": "CPU of Planeptune",
+        "Home": "Planeptune",
+        "Race": "Goddess",
+        "Gender": "Female",
+        "Height": `146 cm (4'09")`,
+        "Weight": "38kg (83.8 lbs)",
+        "Sizes": "B73-W54-H76",
+        "Cup": "A",
+        "Weapon": ["Broadswords", "Katanas"],
+        "Personification": "Sega Console",
+        "Relatives": ["Nepgear"]
+
     },
     'noire':{
-        "Name": "Noire",
+        "English Name": "Noire",
+        "Japanese Name": "ノワール",
+        "Romanji Name": "Nowāru",
         "Nicknames": ["Nowa", "Noiresy", "Loner"],
         "Title": "CPU",
-        "Race": "Goddess"
+        "Occupation": "CPU of Lastation",
+        "Home": "Lastation",
+        "Race": "Goddess",
+        "Gender": "Female",
+        "Height": `158 cm (5'02")`,
+        "Weight": "43kg (94.8 lbs)",
+        "Sizes": "B83-W56-H82",
+        "Cup": "C",
+        "Weapon": ["Short Swords"],
+        "Personification": "Playstation Consoles",
+        "Relatives": ["Uni"]
     },
     'vert':{
-        "Name": "Vert",
+        "English Name": "Vert",
+        "Japanese Name": "ベール",
+        "Romanji Name": "Bēru",
         "Nicknames": ["Bert", "Thunder T*ts", "Vertsy"],
         "Title": "CPU",
-        "Race": "Goddess"
+        "Occupation": "CPU of Leanbox",
+        "Home": "Leanbox",
+        "Race": "Goddess",
+        "Gender": "Female",
+        "Height": `163cm (5'04")`,
+        "Weight": "48kg (105.8 lbs)",
+        "Sizes": "B93-W61-H87",
+        "Cup": "F",
+        "Weapon": ["Spears"],
+        "Personification": "Xbox Consoles",
+        "Relatives": []
     },
     'blanc':{
-        "Name": "Blanc",
+        "English Name": "Blanc",
+        "Japanese Name": "ブラン",
+        "Romanji Name": "Buran",
         "Nicknames": ["Blanny", "Blanny Blanny Bii", "Blancsy"],
         "Title": "CPU",
-        "Race": "Goddess"
+        "Occupation": "CPU of Lowee",
+        "Home": "Lowee",
+        "Race": "Goddess",
+        "Gender": "Female",
+        "Height": `144cm (4'09")`,
+        "Weight": "36kg (79.4 lbs)",
+        "Sizes": "B71-W53-H77",
+        "Cup": "A",
+        "Weapon": ["Hammers"],
+        "Personification": "Nintendo Consoles",
+        "Relatives": ["Ram", "Rom"]
     },
     'unknown':{
         'age': 'unknown',
